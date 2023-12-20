7139ec57acc0ff1d9719ab5f35df484c1a3d7313
Yoimiya <yoimiya@MacbookAir>
2023-12-20 09:27:31 +0800

    23-12-20-0927

--- Codes/KSVD.py
+++ Codes/KSVD.py
@@ -1,4 +1,3 @@
-from typing import Any
 import numpy as np
 import scipy.linalg as la
 
@@ -51,27 +50,58 @@ class DICTLEARN:
         self.max_iter = max_iter
         self.eps = eps
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        return self.DictLearn(*args, **kwds)
+    def __call__(self, patches, mD, mX):
+        return self.DictLearn(patches, mD, mX)
 
 
-    def Phi(self, d, y, x):
-        '''
-        Phi(d) = ||y - d * x||_F^2 / 2
-        '''
-        return la.norm(y - d @ x)**2 / 2
+    def DictLearn(self, patches, mD, mX):
+        for i in range(mD.shape[1]):
+            _indices = np.where(mX[i, :] != 0)[0]
 
-    def GradPhi(self, d, y, x):
+            if _indices.size == 0:
+                continue
+
+            _mY_ = patches[:, _indices]
+            _d = mD[:, i].reshape(-1, 1)
+            _mX_ = mX[i, _indices].reshape(1, -1)
+
+            _d = self.GD(_d, _mY_, _mX_).reshape(-1)
+            mD[:, i] = _d / la.norm(_d)
+        
+        return mD
+    
+    
+    def GD(self, d, mY_, mX_):
+        '''Gradient descent for optimization problem
+        min_d Phi(d)
         '''
-        GradPhi(d) = -2 * (y - d * x) * x.T
-        '''
-        return - (y - d @ x) @ x.T
+        f = lambda d: self.Phi(d, mY_, mX_)
+
+        for _ in range(self.max_iter):
+            _d_tmp, _phi_tmp = d, f(d)
+            _gd = self.GradPhi(d, mY_, mX_)
+
+            d, phi = self.ArmijoLineSearch(f, d, -_gd)
+
+            if la.norm(_d_tmp - d) < self.eps or np.abs((phi - _phi_tmp) / _phi_tmp) < self.eps:
+                break
+        
+        return d
     
 
+    def Phi(self, d, mY_, mX_):
+        '''
+        Phi(d) = ||Y(d) - d * X(d)||_F^2 / 2
+        '''
+        return la.norm(mY_ - d @ mX_)**2 / 2
+
+    def GradPhi(self, d, mY_, mX_):
+        '''
+        GradPhi(d) = - (Y(d) - d * X(d)) * x.T
+        '''
+        return - (mY_ - d @ mX_) @ mX_.T
+    
     def ArmijoLineSearch(self, f, x0, dx):
-        '''
-        Armijo line search
-        '''
         y0 = f(x0)
 
         t = 1
@@ -81,44 +111,8 @@ class DICTLEARN:
                 break
             t *= self.beta
 
-        return t, f(x0 + t * dx)
-
-
-    def GD(self, d_, y_, x_):
-        '''Gradient descent for optimization problem
-        min_d Phi(d)
-        '''
-        f = lambda d: self.Phi(d, y_, x_)
-
-        for _ in range(100):
-            d_tmp = d_
-            phi_tmp = f(d_tmp)
-            grad_d_ = self.GradPhi(d_, y_, x_)
-
-            t, phi = self.ArmijoLineSearch(f, d_, -grad_d_)
-            d_ = d_ - t * grad_d_
-
-            if la.norm(d_tmp - d_) < self.eps or np.abs((phi - phi_tmp) / phi_tmp) < self.eps:
-                break
-        
-        return d_
-
-
-    def DictLearn(self, mD, mX, patches):
-        for i in range(mD.shape[1]):
-            indices = np.where(mX[i, :] != 0)[0]
-
-            if indices.size == 0:
-                continue
-
-            y_ = patches[:, indices]
-            x_ = mX[i, indices].reshape(1, -1)
-            d_ = mD[:, i].reshape(-1, 1)
-
-            d_ = self.GD(d_, y_, x_).reshape(-1)
-            mD[:, i] = d_ / la.norm(d_)
-        
-        return mD
+        _x = x0 + t * dx
+        return _x, f(_x)
 
 
 def KSVD(patches, dictionary, sparsity, _iteration=10, eps=1e-6):
@@ -127,31 +121,18 @@ def KSVD(patches, dictionary, sparsity, _iteration=10, eps=1e-6):
 
     Parameters
     ----------
-    patches : numpy.ndarray
-        The patches
-    sparsity : int
-        The sparsity
-    dictionary : numpy.ndarray
-        The dictionary
-    max_iter : int
-        The maximum number of iterations
-    eps : float
-        The threshold
-
+    
     Returns
     -------
-    mD : numpy.ndarray
-        The dictionary
-    mX : numpy.ndarray
-        The sparse representation
+    
     '''
     DictLearn = DICTLEARN(eps=eps)
 
     mD = dictionary
-    mX = OMP(patches, mD, sparsity, eps)
 
     for _ in range(_iteration):
-        mD = DictLearn(mD, mX, patches)
-        mX = OMP(mD, patches, sparsity, eps)
+        if _ > 0:
+            mD = DictLearn(patches, mD, mX)
+        mX = OMP(patches, mD, sparsity, eps)
     
-    return mD @ mX, mD, mX
+    return mD, mX
